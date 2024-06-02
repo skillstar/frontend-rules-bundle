3d83f5366f5db5b847b3cc92a468080c0e742acb
yangsheng8 <344182792@qq.com>
2024-06-02 10:33:08 +0800

    feat: add docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,8 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+ import './style.css'
+ import './custom.css'
 
 export default {
   extends: DefaultTheme,
@@ -12,6 +13,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+     //...
   }
 } satisfies Theme
